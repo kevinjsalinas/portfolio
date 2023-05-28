f497f5be6e85193ee70a222f42f14b4641ee23a4
Kevin Salinas <kevinsalinas155@gmail.com>
2023-05-28 19:42:26 -0400

    added side bar nav base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,27 @@
-import logo from './logo.svg';
-// import './App.css' lol;
+import { useState } from 'react'
+import { BsArrowLeftShort } from "react-icons/bs";
 
 function App() {
+
+  const [open, setOpen] = useState(true)
+  
+  //fyi: ALEX used a regular side nav bar using something similar to tailwind css 
+  //or regular css, i don't know...
+
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold">
-      Hello world!
-      </h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="flex">
+      <div 
+        className={`bg-dark-purple h-screen p-5 pt-8 ${open ? "w-72" : "w-20"} relative`}>
+        <BsArrowLeftShort 
+          className="bg-white text-dark-purple text-3xl rounded-full absolute -right-3 top-9 border border-dark-purple cursor-pointer" 
+          onClick={() => setOpen(!open)}
+        />
+      </div>
+      <div className="p-7"><h1 className="text-2xl font-semibold">Home Page</h1></div>
+
     </div>
+    
   );
 }
 
