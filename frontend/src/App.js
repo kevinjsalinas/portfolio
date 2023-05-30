1e31a735176e887e9e6b5787fb8b72badd8369c0
Kevin Salinas <kevinsalinas155@gmail.com>
2023-05-29 22:58:05 -0400

    added ks logo to nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { BsArrowLeftShort } from "react-icons/bs";
+// import { VscHome } from "react-icons/vsc";
+
 
 function App() {
 
@@ -12,11 +14,15 @@ function App() {
 
     <div className="flex">
       <div 
-        className={`bg-dark-purple h-screen p-5 pt-8 ${open ? "w-72" : "w-20"} duration-300 relative`}>
+        className={`bg-dark-purple h-screen p-5 pt-8 ${open ? "w-72" : "width:5rem"} duration-300 relative`}>
         <BsArrowLeftShort 
           className={`bg-white text-dark-purple text-3xl rounded-full absolute -right-3 top-9 border border-dark-purple cursor-pointer ${!open && "rotate-180"}`} 
           onClick={() => setOpen(!open)}
         />
+        <div className="inline-flex">
+          <h1 className="bg-amber-300 text-4xl rounded cursor-pointer block float-left mr-2">ks</h1>
+          <h1 className={`text-white origin-left font-medium text-2xl duration-300 ${!open && "scale-0"}`}>{!open ? "" : "Kevin Salinas"}</h1>
+        </div>
       </div>
       <div className="p-7"><h1 className="text-2xl font-semibold">Home Page</h1></div>
 
