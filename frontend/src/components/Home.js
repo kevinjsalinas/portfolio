ad0cafb9190289192a4b351b503ea3b5c9aea907
Kevin Salinas <kevinsalinas155@gmail.com>
2023-06-08 16:54:07 -0400

    created home section with added styling

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,11 +5,20 @@ function Home() {
 
     return (
     
-        <div class="container mx-auto p-7 flex-wrap center">
-            <div className="image-cropper">
-                <img className="rounded" src="/salinas_kevin.jpeg" alt="salinas_kevin_picture"/>
+        <div name="home" className="h-screen w-full bg-gradient-to-b from-white via-white to-gray-400">
+            <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+                <div className="flex flex-col justify-center h-full">
+                    <h2 className="text-4xl sm:text-7xl font-bold">Hi there! I'm Kevin Salinas</h2>
+                    <p className="py-4 max-w-md"> A Full-Stack Web Developer That Enjoys Transforming Ideas Into Tangible, Real-World Applications.</p>
+                    <div className="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+                        <button>Resume</button>
+                    </div>
+                </div>
+                <div>
+                    <img className="rounded-2xl mx-auto w-2/6 md:w-full" src="/salinas_kevin.jpeg" alt="salinas_kevin_picture"/>
+                </div>
+
             </div>
-            <h1 className="pl-10 mr-15 text-xl font-semibold">HI THERE! I'M</h1>
         </div>
   
     )
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -23,13 +23,13 @@ function NavBar() {
 
         <div>
             <div 
-            className={`bg-dark-purple h-screen p-5 pt-8 ${open ? "w-72" : "width:5rem"} duration-300 relative`}>
+            className={`bg-blue-600 h-screen p-5 pt-8 ${open ? "w-72" : "width:5rem"} duration-300 relative`}>
             <BsArrowLeftShort 
                 className={`bg-white text-dark-purple text-3xl rounded-full absolute -right-3 top-9 border border-dark-purple cursor-pointer ${!open && "rotate-180"}`} 
                 onClick={() => setOpen(!open)}
             />
             <div className="inline-flex">
-                <h1 className={`bg-amber-300 text-4xl rounded cursor-pointer block float-left mr-2 duration-500 ${open && "rotate-[360deg]"}`}>ks</h1>
+                <h1 className={`bg-amber-200 text-4xl rounded cursor-pointer block float-left mr-2 duration-500 ${open && "rotate-[360deg]"}`}>ks</h1>
                 <h1 className={`text-white origin-left font-medium text-2xl duration-300 ${!open && "scale-0"}`}>{!open ? "" : "Kevin Salinas"}</h1>
             </div>
             <ul className='pt-2'>
